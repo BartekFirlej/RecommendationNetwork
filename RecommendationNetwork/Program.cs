119983a9ae48c5656a0d99311198d7a7bb3e0f35
Firlej Bartłomiej <yari12@wp.pl>
2023-12-17 20:32:55 +0100

    Created neo4j db connection and swagger

--- RecommendationNetwork/Program.cs
+++ RecommendationNetwork/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<Neo4jService>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var neo4jUri = configuration.GetConnectionString("Neo4jConnection");
+    var neo4jUser = configuration.GetConnectionString("Neo4jUser");
+    var neo4jPassword = configuration.GetConnectionString("Neo4jPassword");
+
+    return new Neo4jService(neo4jUri, neo4jUser, neo4jPassword);
+});
+
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World");
 
 app.Run();
