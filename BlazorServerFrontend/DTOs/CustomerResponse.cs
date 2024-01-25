371d92f80f273d0ee80f63caf6b8c811c840dd9e
Firlej Bartłomiej <yari12@wp.pl>
2024-01-25 10:08:58 +0100

    Created blazor server proejct and first endpoint fetch

---
+++ BlazorServerFrontend/DTOs/CustomerResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorServerFrontend.DTOs
+{
+    public class CustomerResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Town { get; set; } = null!;
+
+        public string ZipCode { get; set; } = null!;
+
+        public string Street { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+
+        public int VoivodeshipId { get; set; }
+        public int? RecommenderId { get; set; }
+    }
+}
---
+++ BlazorServerFrontend/Services/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using BlazorServerFrontend.DTOs;
+
+namespace BlazorServerFrontend.Services
+{
+    public class CustomerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CustomerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<CustomerResponse>> GetCustomersAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<CustomerResponse>>("http://localhost:8082/customers");
+        }
+    }
+}
