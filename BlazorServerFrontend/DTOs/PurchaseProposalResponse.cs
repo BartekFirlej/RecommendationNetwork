76fdf0290b5b88d4fadcc68a3927de9ccbe9de5f
Firlej Bartłomiej <yari12@wp.pl>
2024-01-27 00:13:11 +0100

    Added purchase proposal product and product type name on website

--- BlazorServerFrontend/DTOs/PurchaseProposalResponse.cs
+++ BlazorServerFrontend/DTOs/PurchaseProposalResponse.cs
@@ -5,5 +5,8 @@
         public DateTimeOffset Date { get; set; }
         public int CustomerId { get; set; }
         public int ProductId { get; set; }
+        public int ProductTypeId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductTypeName { get; set; }
     }
 }
